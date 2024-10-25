830be9cc3f038a21871726557580f6559cc1d6f1
Edison <edison@Edisons-MacBook-Air.local>
2024-10-24 20:13:32 -0500

    Fixing test stuffs

--- tests/unit/test_glue_migration_stack.py
+++ tests/unit/test_glue_migration_stack.py
@@ -1,31 +1,34 @@
+# tests/unit/test_glue_migration_stack.py
 import aws_cdk as cdk
 import aws_cdk.assertions as assertions
 from glue_migration_project.glue_migration_stack import GlueMigrationStack
-import pytest
 
 def test_s3_bucket_created():
     # GIVEN
     app = cdk.App()
-    
     # WHEN
     stack = GlueMigrationStack(app, "TestStack", env_name="test")
     template = assertions.Template.from_stack(stack)
-
     # THEN
-    template.has_resource_properties("AWS::S3::Bucket", {
-        "VersioningConfiguration": {
-            "Status": "Enabled"
-        }
+    template.has_resource_properties("AWS::S3::Bucket", {})
+
+def test_rds_instance_created():
+    # GIVEN
+    app = cdk.App()
+    # WHEN
+    stack = GlueMigrationStack(app, "TestStack", env_name="test")
+    template = assertions.Template.from_stack(stack)
+    # THEN
+    template.has_resource_properties("AWS::RDS::DBInstance", {
+        "Engine": "postgres"
     })
 
 def test_glue_job_created():
     # GIVEN
     app = cdk.App()
-    
     # WHEN
     stack = GlueMigrationStack(app, "TestStack", env_name="test")
     template = assertions.Template.from_stack(stack)
-
     # THEN
     template.has_resource_properties("AWS::Glue::Job", {
         "Command": {
@@ -38,10 +41,37 @@ def test_glue_job_created():
 def test_secret_created():
     # GIVEN
     app = cdk.App()
-    
     # WHEN
     stack = GlueMigrationStack(app, "TestStack", env_name="test")
     template = assertions.Template.from_stack(stack)
-
     # THEN
-    template.has_resource("AWS::SecretsManager::Secret")
+    template.has_resource_properties("AWS::SecretsManager::Secret", {
+        "GenerateSecretString": {
+            "SecretStringTemplate": '{"username": "postgres", "dbname": "migration_db"}'
+        }
+    })
+
+def test_vpc_created():
+    # GIVEN
+    app = cdk.App()
+    # WHEN
+    stack = GlueMigrationStack(app, "TestStack", env_name="test")
+    template = assertions.Template.from_stack(stack)
+    # THEN
+    template.has_resource_properties("AWS::EC2::VPC", {
+        "EnableDnsHostnames": True,
+        "EnableDnsSupport": True
+    })
+
+def test_glue_connection_created():
+    # GIVEN
+    app = cdk.App()
+    # WHEN
+    stack = GlueMigrationStack(app, "TestStack", env_name="test")
+    template = assertions.Template.from_stack(stack)
+    # THEN
+    template.has_resource_properties("AWS::Glue::Connection", {
+        "ConnectionInput": {
+            "ConnectionType": "JDBC"
+        }
+    })
